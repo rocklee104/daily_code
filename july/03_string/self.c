23ccc66545ef5eb03dc1c97865094d25067e4201
rli <rli@sierrawireless.com>
2017-03-18 20:27:36 +0800

    3.18

---
+++ july/03_string/self.c
@@ -0,0 +1,131 @@
+/*
+ * 字母的全排序,字母长度可能会变化,如果用循环来做,循环的嵌套层次也需要变化,不太容易实现
+ */
+#include <stdio.h>
+#include <string.h>
+#include <assert.h>
+#include <stdlib.h>
+
+struct test {
+    struct test *pre;
+    struct test *next;
+    char *array;
+    int *set;
+    int len;
+};
+
+struct head {
+    struct test head;
+};
+
+struct head g_head;
+
+int head_init(struct head* head)
+{
+    head->head.next = &head->head;
+    head->head.pre = &head->head;
+}
+
+int list_add(struct head *head, struct test *node)
+{
+    node->next = &head->head;
+    node->pre = head->head.pre;
+    head->head.pre->next = node;
+    head->head.pre = node;
+}
+
+int list_delete(struct head *head, struct test *node)
+{
+    node->pre->next = node->next;
+    node->next->pre = node->pre;
+    node->next = NULL;
+    node->pre = NULL;
+}
+
+void list_destroy(struct head *head)
+{
+    struct test *node = head->head.pre;
+
+    while (node != &head->head) {
+        list_delete(head, node);
+
+        free(node->array);
+        free(node);
+        node = head->head.pre;
+    }
+}
+
+struct test* alloc_node(int len)
+{
+    struct test  *node = (struct test *)malloc(sizeof(struct test));
+    if (!node)
+        return node;
+    memset(node, 0, sizeof(struct test));
+
+    node->len = len;
+    node->array = malloc(sizeof(char) * len);
+    if (!node->array)
+        goto out1;
+    memset(node->array, 0, sizeof(char) * len);
+
+    node->set = malloc(sizeof(char) * len);
+    if (!node->set)
+        goto out2;
+    memset(node->set, 0, sizeof(char) * len);
+
+    return node;
+out2:
+    free(node->array);
+out1:
+    free(node);
+    return NULL;
+}
+
+void traverse_list(struct head *head)
+{
+    struct test *node = head->head.next;
+    while (node != &(head->head)) {
+        printf("%s\n", node->array);
+        node = node->next;
+    }
+}
+
+void copy_node(struct test *pre, struct test *node)
+{
+    memcpy(node->array, pre->array, pre->len);
+    memcpy(node->set, pre->set, pre->len);
+}
+
+int all_permutation(char *s, int len, int pos, struct test *pre)
+{
+    int i;
+    struct test *node;
+
+    for (i = 0; i < len; i++) {
+        node = alloc_node(len);
+        assert(node);
+        //memcpy(node->array, s, len);
+
+        if (!pre)
+            copy_node(pre, node);
+
+        node->set[pos] = i;
+        node->array[pos] = s[i];
+        list_add(&g_head, node);
+
+        all_permutation(s, len, pos++, node);
+    }
+    return 0;
+}
+
+int main(void)
+{
+    char s[] = "abcd";
+    int len = strlen(s);
+
+    head_init(&g_head);
+    all_permutation(s, len, 0, NULL);
+
+    traverse_list(&g_head);
+    list_destroy(&g_head);
+}
